fd46541bc9b498131b2f7d1a4a433cae7a3c059b
Alef <oa.g11@icloud.com>
2021-11-11 22:33:36 -0600

    Extract and minify css, add terser JS, minify HTML

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,7 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: "./src/index.js",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-        'style-loader', //3. Inject styles into DOM
-        'css-loader',   //2. Turns css into common js
-        'sass-loader',   //1. Turns sass into css
-        ]
-      },
       {
         test: /\.html$/i,
         loader: "html-loader",
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const common = require('./webpack.common')
 const { merge } = require('webpack-merge')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "production",
@@ -10,5 +14,31 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: "./img/[name].[hash].[ext]",
   },
-  plugins: [new CleanWebpackPlugin()]
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/template.html',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace:true,
+          removeComments:true
+        }
+      })
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename:"[name].[contenthash].css"}),
+    new CleanWebpackPlugin()
+  ],
 });
