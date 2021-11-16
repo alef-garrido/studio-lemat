1873e50db7336cb715b6621533c77450cc3a8cbf
alef-garrido <oa.g11@icloud.com>
2021-11-16 17:01:46 -0600

    Add hero section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.scss"
+
+const navContainer = document.getElementById('navContainer')
+const menuIcon = document.getElementsByClassName('menuIcon')
+let iconsM = []
+Array.from(menuIcon, (e) => iconsM.push(e))
+
+navContainer.addEventListener('click', (e) => {
+  if(e.target.classList.contains('menuIcon')){
+    const menuContainer = document.getElementById('menuContainer')
+    menuContainer.classList.toggle('hide')
+  }
+})
