db6ac331c1aec5162c5b43810f1af2a99629ade7
alef-garrido <oa.g11@icloud.com>
2021-11-19 15:11:37 -0600

    Fix mobile menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import "./style.scss"
 
 const navContainer = document.getElementById('navContainer')
-const menuIcon = document.getElementsByClassName('menuIcon')
-let iconsM = []
-Array.from(menuIcon, (e) => iconsM.push(e))
+const menuContainer = document.getElementById('menuContainer')
+const menuBtns = document.querySelector('.menuBtns')
 
-navContainer.addEventListener('click', (e) => {
-  if(e.target.classList.contains('menuIcon')){
-    const menuContainer = document.getElementById('menuContainer')
+const myToggle = function(event, str){
+  if(event.target.classList.contains(str)){
+    console.log(event)
     menuContainer.classList.toggle('hide')
   }
-})
+}
+
+navContainer.addEventListener('click', (e) => myToggle(e, 'menuIcon'))
+menuBtns.addEventListener('click', (e) => myToggle(e, 'closeBtn'))
